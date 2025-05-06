4f254a5cfdb8ad3abc0d557f32e8bde221732b09
Gydetti <gydo@groeienmetgydo.nl>
2025-05-06 07:35:03 +0200

    fix(button): add more vertical padding for multi-line button content

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,9 +20,9 @@ const buttonVariants = cva(
 				link: "text-primary underline-offset-4 hover:underline",
 			},
 			size: {
-				default: "px-4 py-3",
-				sm: "h-9 rounded-md px-3",
-				lg: "h-11 rounded-md px-8",
+				default: "px-4 py-4",
+				sm: "px-3 py-2 rounded-md",
+				lg: "px-8 py-4 rounded-md",
 				icon: "h-10 w-10",
 			},
 		},
